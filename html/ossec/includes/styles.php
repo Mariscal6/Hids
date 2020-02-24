08560e9bf7deb8e5a2978085cbf506cb11087b23
MiniNanook <adruiz01@ucm.es>
2020-02-25 00:43:03 +0100

    Primera Revisión HTML

---
+++ html/ossec/includes/styles.php
@@ -0,0 +1,7 @@
+<?php
+echo '<link href="css/sb-admin-2.min.css" rel="stylesheet" type="text/css">';
+echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
+echo '<link rel="stylesheet" href="css/sb-admin-2.min.css" rel="stylesheet" type="text/css">';
+echo '<link rel="stylesheet" href="css/mdb.min.css" rel="stylesheet" type="text/css">';
+echo '<link rel="stylesheet" href="css/bootstrap.min.css" rel="stylesheet" type="text/css">';
+?>
---
+++ html/ossec/integrity.php
@@ -0,0 +1,23 @@
+<?php include("includes/config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include("includes/styles.php");?>
+</head>
+<body>
+
+<?php include("includes/header.php");?>
+
+<div>
+	<div class="row justify-content-center">
+		<div clas="col-12">
+			<br>
+			<h1>Integrity</h1>
+		</div>
+	</div>
+</div>
+
+<?php include("includes/footer.php");?>
+<?php include("includes/scripts.php");?>
+</body>
+</html>
