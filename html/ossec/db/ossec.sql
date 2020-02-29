5a0001ba8d8b8539b39fcee9d20c416ecc9f8d68
MiniNanook <adruiz01@ucm.es>
2020-02-29 22:54:44 +0100

    Add Create DB

--- html/ossec/db/ossec.sql
+++ html/ossec/db/ossec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-02-2020 a las 22:47:59
+-- Tiempo de generación: 29-02-2020 a las 22:53:56
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.2
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ossec`
 --
+CREATE DATABASE IF NOT EXISTS `ossec` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `ossec`;
 
 -- --------------------------------------------------------
 
